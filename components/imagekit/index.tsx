fe470b8b35e37a8765c4f0340eaf43a8c683834b
casulit <chrisgelosulit@gmail.com>
2024-02-04 22:25:23 +0800

    refactor(components/imagekit): Extract ImageKitLoaderProps interface

--- components/imagekit/index.tsx
+++ components/imagekit/index.tsx
@@ -5,15 +5,23 @@ import Image from "next/image";
 
 import { imageKitLoader } from "@/lib/utils";
 
-export const ImageKitLoader: React.FC<
-  React.ImgHTMLAttributes<HTMLImageElement>
-> = ({ className, src, alt }) => {
+export interface ImageKitLoaderProps
+  extends React.ImgHTMLAttributes<HTMLImageElement> {
+  src: string;
+  alt: string;
+}
+
+export const ImageKitLoader: React.FC<ImageKitLoaderProps> = ({
+  className,
+  src,
+  alt,
+}) => {
   return (
     <Image
       className={className}
       loader={imageKitLoader}
-      src={src!}
-      alt={alt!}
+      src={src}
+      alt={alt}
       fill={true}
       priority={true}
       sizes="(max-width: 768px) 100vw, 700px"
