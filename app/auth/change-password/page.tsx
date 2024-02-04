4cebbd48387d00c76a4c3fc99daccfa356279c3b
casulit <chrisgelosulit@gmail.com>
2024-02-04 19:47:27 +0800

    refactor(auth): extract form title to AuthFormTitle component

--- app/auth/change-password/page.tsx
+++ app/auth/change-password/page.tsx
@@ -1,21 +1,18 @@
 import React from "react";
 
 import { ChangePasswordForm } from "@/components/pages/auth/change-password/change-password-form";
+import { AuthFormTitle } from "@/components/pages/auth/form-title";
 
 const ChangePasswordPage = async () => {
   // TODO: Make a server call here to check if reset password token is valid
   return (
     <div className="flex min-h-screen items-center justify-center">
       <div className="w-full max-w-md space-y-6 rounded bg-white p-8">
-        <div className="space-y-2.5">
-          <h1 className="text-2xl font-bold leading-10 text-gray-900">
-            Reset your password
-          </h1>
-          <p className="text-sm leading-snug text-gray-500">
-            Almost done! Create a strong password, enter it, and you&apos;re
-            good to go.
-          </p>
-        </div>
+        <AuthFormTitle
+          mainHeading="Reset your password"
+          subHeading="Almost done! Create a strong password, enter it, and you're
+            good to go."
+        />
         <ChangePasswordForm />
       </div>
     </div>
