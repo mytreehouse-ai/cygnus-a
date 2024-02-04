4bdb5e918bc79addc41355a00d80bc4c563ad154
casulit <chrisgelosulit@gmail.com>
2024-02-04 20:17:06 +0800

    feat(layout): Integrate ReactQueryProvider and Devtools, update success msg

--- components/pages/auth/forgot-password/form-submit-success-message.tsx
+++ components/pages/auth/forgot-password/form-submit-success-message.tsx
@@ -1,5 +1,29 @@
-import React from "react";
+"use client";
 
-export const FormSubmitSuccessMessage = () => {
-  return <div>FormSubmitSuccessMessage</div>;
+import React from "react";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import { AuthFormTitle } from "../form-title";
+
+interface FormSubmitSuccessMessageProps {
+  email: string;
+}
+
+export const FormSubmitSuccessMessage: React.FC<
+  FormSubmitSuccessMessageProps
+> = ({ email }) => {
+  return (
+    <>
+      <AuthFormTitle
+        mainHeading="Email on the way!"
+        subHeading={`We sent you password reset instructions to ${email}. If it doesn't show up soon, check your spam folder. We sent it from the email address noreply@address.here`}
+      />
+      <Link href="/auth/login">
+        <Button className="w-full" size="lg" type="button">
+          Return to login
+        </Button>
+      </Link>
+    </>
+  );
 };
