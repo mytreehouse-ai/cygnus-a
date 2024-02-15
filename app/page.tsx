22b17b3602b81f84a6d3fa899aa44b7c6341bdcb
Seth Maño <joshuaseth11@gmail.com>
2024-02-15 21:44:39 +0800

    UI for searchbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import SearchBar from "@/components/pages/home/search-bar";
+
 export default function Home() {
-  return <div>Nothing is here...TEST</div>;
+  return (
+    <>
+      <div className="bg-emerald-50 p-4">
+        <SearchBar />
+      </div>
+    </>
+  );
 }
