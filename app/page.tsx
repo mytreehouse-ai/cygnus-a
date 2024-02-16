7a64e951db7378da82d4dab28f503cf77960e9ae
Seth Maño <joshuaseth11@gmail.com>
2024-02-16 10:34:17 +0800

    added property card

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
 import SearchBar from "@/components/pages/home/search-bar";
+import PropertyCard from "@/components/pages/home/property-card";
 
 export default function Home() {
   return (
     <>
       <div className="bg-emerald-50 p-4">
         <SearchBar />
+        {/* <PropertyCard
+          img="/property-image.png"
+          location="Paseo de Roxas, Makati"
+          price={146000}
+          sqm={304}
+          propertyName="Furnished Condominium Unit"
+          propertyType="Condominium"
+        /> */}
       </div>
     </>
   );
