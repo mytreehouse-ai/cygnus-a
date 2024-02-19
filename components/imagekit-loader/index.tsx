30f3a0b14e256bb3a3e7fae301cb0ffe16c37292
casulit <chrisgelosulit@gmail.com>
2024-02-19 13:31:06 +0800

    refactor(components/imagekit-loader): Extract type definitions for image props

--- components/imagekit-loader/index.tsx
+++ components/imagekit-loader/index.tsx
@@ -6,6 +6,24 @@ import Image from "next/image";
 import { imageKitLoader } from "@/lib/utils";
 
 /**
+ * Type definition for responsive images.
+ * This type is used when the image should automatically adjust to the container's width.
+ */
+type TResponsive = {
+  responsive: true;
+  sizes?: string;
+};
+
+/**
+ * Type definition for non-responsive images.
+ * This type is used when the image has fixed dimensions, requiring explicit width and height values.
+ */
+type TNoneResponsive = {
+  responsive: false;
+  width: number; // The width of the image in pixels.
+  height: number; // The height of the image in pixels.
+};
+/**
  * Defines the properties for the ImageKitLoader component.
  * @property {string} [className] - Optional CSS class to apply to the image.
  * @property {string} src - The source URL of the image.
@@ -18,16 +36,7 @@ type TImageKitLoaderProps = {
   className?: string;
   src: string;
   alt: string;
-} & (
-  | {
-      responsive: true;
-    }
-  | {
-      responsive: false;
-      width: number;
-      height: number;
-    }
-);
+} & (TResponsive | TNoneResponsive);
 
 /**
  * A component that renders an image using the ImageKit service with support for responsive and fixed dimensions.
@@ -46,7 +55,7 @@ export function ImageKitLoader(props: TImageKitLoaderProps): JSX.Element {
       alt={alt}
       fill={true}
       priority={true}
-      sizes="(max-width: 768px) 100vw, 700px"
+      sizes={props.sizes ? props.sizes : "(max-width: 768px) 100vw, 700px"}
     />
   ) : (
     <Image
