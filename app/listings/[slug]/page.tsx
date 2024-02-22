c5f8ecb199a49a276c632b20c5f5eafba8601741
Seth Maño <joshuaseth11@gmail.com>
2024-02-22 22:41:48 +0800

    feat: added dynamic listing details page

---
+++ app/listings/[slug]/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import PropertyDetails from "@/components/listings/details/page";
+
+const Details = () => {
+  return <PropertyDetails />;
+};
+
+export default Details;
