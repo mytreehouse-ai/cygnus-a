1d783421289129718b5b1b191af292198f75ac7c
Seth Maño <joshuaseth11@gmail.com>
2024-02-23 23:20:25 +0800

    added ui form contact property details

--- components/listings/details/page.tsx
+++ components/listings/details/page.tsx
@@ -17,7 +17,11 @@ import Image from "next/image";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { Shrink, Expand } from "lucide-react";
-import Head from "next/head";
+import { useForm } from "react-hook-form";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
 
 const propertyDetails = {
   floorArea: 45,
@@ -34,7 +38,7 @@ const propertyDetails = {
 const PropertyDetails = () => {
   return (
     <>
-      <main className="min-h-screen px-4 pb-4 pt-10">
+      <main className="min-h-screen space-y-6 px-4 pb-4 pt-10">
         <section className="space-y-1">
           <h3 className="text-2xl font-bold">Furnished Condominium Unit</h3>
           <Badge className={cn(" rounded-md bg-orange-400")}>For Rent</Badge>
@@ -42,10 +46,10 @@ const PropertyDetails = () => {
             <MapPin className="h-4 w-4 text-slate-500" />
             <span> Paseo de Roxas, Makati</span>
           </p>
+          <h3 className="text-3xl font-bold text-emerald-600">$146,000</h3>
         </section>
-        <h3 className="text-3xl font-bold text-emerald-600">$146,000</h3>
 
-        <section className="grid-rows-auto mt-6 grid grid-cols-2 gap-2">
+        <section className="grid-rows-auto  grid grid-cols-2 gap-2">
           <div className="relative col-span-2 h-auto w-full max-w-[500px]">
             <Image
               src="/dummy-img.png"
@@ -78,7 +82,7 @@ const PropertyDetails = () => {
           </div>
         </section>
 
-        <section className="mt-6">
+        <section>
           <Card className="border-none shadow">
             <CardHeader>
               <CardTitle className="text-lg text-gray-950">Overview</CardTitle>
@@ -164,7 +168,7 @@ const PropertyDetails = () => {
           </Card>
         </section>
 
-        <section className="mt-6">
+        <section>
           <Card className="border-none shadow">
             <CardHeader>
               <CardTitle className="text-lg text-gray-950">
@@ -182,7 +186,7 @@ const PropertyDetails = () => {
           </Card>
         </section>
 
-        <section className="mt-6">
+        <section>
           <Card className="border-none shadow">
             <CardHeader>
               <CardTitle className="inline-flex items-center gap-x-2 text-lg text-gray-950">
@@ -198,9 +202,115 @@ const PropertyDetails = () => {
             </CardContent>
           </Card>
         </section>
+
+        <ContactUs />
       </main>
     </>
   );
 };
 
 export default PropertyDetails;
+
+const ContactUs = () => {
+  const contactUsForm = useForm();
+
+  const onSubmit = () => {
+    console.log("submit");
+  };
+
+  return (
+    <section>
+      <Card className="border bg-neutral-50 shadow-sm">
+        <CardHeader>
+          <CardTitle className="inline-flex items-center gap-x-2 text-lg text-gray-950">
+            Contact Us Now!
+          </CardTitle>
+          <p className="text-xs font-normal text-slate-500">
+            Paseo de Roxas, Makati
+          </p>
+        </CardHeader>
+        <CardContent>
+          <Form {...contactUsForm}>
+            <form
+              name="search-property-form"
+              onSubmit={contactUsForm.handleSubmit(onSubmit)}
+              className="flex flex-col gap-y-2"
+            >
+              <FormField
+                control={contactUsForm.control}
+                name="bathroom"
+                render={({ field }) => (
+                  <FormItem className="w-auto md:w-full">
+                    <FormControl>
+                      <Input
+                        placeholder="Full Name"
+                        {...field}
+                        value={field.value}
+                        className="w-full rounded-lg text-sm md:w-full"
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={contactUsForm.control}
+                name="bathroom"
+                render={({ field }) => (
+                  <FormItem className="w-auto md:w-full">
+                    <FormControl>
+                      <Input
+                        placeholder="Contact Number"
+                        {...field}
+                        value={field.value}
+                        className="w-full rounded-lg text-sm md:w-full"
+                        type="number"
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={contactUsForm.control}
+                name="bathroom"
+                render={({ field }) => (
+                  <FormItem className="w-auto md:w-full">
+                    <FormControl>
+                      <Input
+                        placeholder="Address"
+                        {...field}
+                        value={field.value}
+                        className="w-full rounded-lg text-sm md:w-full"
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={contactUsForm.control}
+                name="bathroom"
+                render={({ field }) => (
+                  <FormItem className="w-auto md:w-full">
+                    <FormControl>
+                      <Textarea
+                        placeholder="Message"
+                        {...field}
+                        value={field.value}
+                        className="w-full rounded-lg text-sm md:w-full"
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <Button className="order-2 mt-3 w-full rounded-lg bg-emerald-600 hover:bg-emerald-700 focus:bg-emerald-600 active:bg-emerald-600 md:order-1">
+                Inquire
+              </Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </section>
+  );
+};
