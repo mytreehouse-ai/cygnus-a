0ba21a8e28073c2c1e24a5bb75cd1024f6206844
casulit <chrisgelosulit@gmail.com>
2024-02-25 10:45:02 +0800

    style(global/footer): Update footer styling and content

--- components/global/footer/index.tsx
+++ components/global/footer/index.tsx
@@ -14,7 +14,11 @@ function Footer() {
           alt=""
         />
       </div>
-      <div className="border border-gray-300">Other footer style here</div>
+      <footer className="border border-gray-300 bg-slate-950 py-4 text-center font-semibold text-white">
+        &copy; {new Date().getFullYear()}{" "}
+        <span className="text-emerald-600">MyTreeHouse</span>. All rights
+        reserved.
+      </footer>
     </Fragment>
   );
 }
