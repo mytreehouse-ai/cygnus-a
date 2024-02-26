fa3b088aaa3a954e852f5f78a514208f046e58fd
Seth Maño <joshuaseth11@gmail.com>
2024-02-26 22:51:55 +0800

    added: valuation page

---
+++ components/property-valuation/index.tsx
@@ -0,0 +1,62 @@
+"use client";
+import z from "zod";
+import { Card, CardContent } from "@/components/ui/card";
+import { Progress } from "@/components/ui/progress";
+import ValuationStepper from "@/hooks/useStepperStore";
+import PropertyDetails from "./property-details";
+// import PersonalDetails from "./personaldetails";
+// import ValuationResults from "./valuationresults";
+
+const componentsMap = {
+  Property: PropertyDetails,
+  // Personal: PersonalDetails,
+  // Results: ValuationResults,
+};
+
+export const propertyValuationFormSchema = z.object({
+  propertyType: z.string().nonempty(),
+  address: z.string().nonempty(),
+  location: z.string().uuid(),
+  sqm: z.preprocess((val) => Number(val), z.number().positive()),
+  yearBuilt: z.string().nonempty(),
+  whenAreyouLookingToSell: z.string().nonempty(),
+});
+
+export const personalDetailsFormSchema = z.object({
+  lastName: z.string().nonempty(),
+  firstName: z.string().nonempty(),
+  phoneNumber: z.string().nonempty(),
+  emailAddress: z.string().email().nonempty(),
+  termsAndConditions: z.boolean().refine((val) => val === true),
+  offers: z.boolean().optional(),
+});
+
+const PropertyValuation: React.FC = () => {
+  const { steps, currentStepIndex, markStepAsComplete } = ValuationStepper();
+
+  const ComponentToRender =
+    componentsMap[
+      steps[currentStepIndex]?.name as keyof typeof componentsMap
+    ] || null;
+
+  return (
+    <Card className="relative mx-auto w-full min-w-[320px] max-w-[1000px] overflow-y-auto rounded-xl md:min-w-[480px] md:rounded-xl">
+      <div className="sticky left-0 top-0 z-10 flex h-20 w-full items-center justify-center bg-white px-6">
+        <Progress
+          value={
+            currentStepIndex === 1
+              ? 33.33
+              : currentStepIndex === 2
+                ? 100
+                : currentStepIndex === 3
+                  ? 100
+                  : 0
+          }
+        />
+      </div>
+      <CardContent>{ComponentToRender && <ComponentToRender />}</CardContent>
+    </Card>
+  );
+};
+
+export default PropertyValuation;
---
+++ components/property-valuation/property-details.tsx
@@ -0,0 +1,190 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import ValuationStepper from "@/hooks/useStepperStore";
+import { propertyValuationFormSchema } from ".";
+import useValuationFormStore from "@/hooks/useValuationFormStore";
+import { propertyTypes } from "@/static_data/property-types";
+import { yearBuilt } from "@/static_data/year-built";
+import { listingDuration } from "@/static_data/listing-duration";
+// import { CityCombobox } from "@/components/ui/citycombobox";
+
+const PropertyDetails: React.FC = () => {
+  const { currentStepIndex, setCurrentStepIndex, steps } = ValuationStepper();
+  const { propertyDetailValues, setPropertyDetailValues } =
+    useValuationFormStore();
+
+  const form = useForm<z.infer<typeof propertyValuationFormSchema>>({
+    resolver: zodResolver(propertyValuationFormSchema),
+    values: propertyDetailValues,
+  });
+
+  const onSubmit = (values: z.infer<typeof propertyValuationFormSchema>) => {
+    setPropertyDetailValues(values);
+    console.log("values set", values);
+    if (currentStepIndex < steps.length - 1) {
+      setCurrentStepIndex(+1);
+    }
+  };
+
+  return (
+    <Form {...form}>
+      <h2 className="w-full text-lg font-bold text-neutral-800">
+        Tell us about your property
+      </h2>
+
+      <form
+        name="propertyDetailsForm"
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="mt-4 space-y-4"
+      >
+        <FormField
+          control={form.control}
+          name="propertyType"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Property type</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Property type" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  {propertyTypes.map((pt) => (
+                    <SelectItem key={pt.value} value={pt.value}>
+                      {pt.label}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="address"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Address</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="Address"
+                  {...field}
+                  value={field.value ?? ""}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="location"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Location</FormLabel>
+              {/* <CityCombobox
+                onCityChange={field.onChange}
+                cityValue={field.value}
+              /> */}
+            </FormItem>
+          )}
+        />
+
+        <div className="w-full md:flex md:gap-x-6">
+          <FormField
+            control={form.control}
+            name="sqm"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormLabel>Sqm</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Sqm"
+                    type="number"
+                    min="1"
+                    {...field}
+                    value={field.value ?? ""}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="yearBuilt"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormLabel>Year built</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Year built" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    {yearBuilt.map((data) => (
+                      <SelectItem key={data.value} value={data.value}>
+                        {data.label}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              </FormItem>
+            )}
+          />
+        </div>
+        <FormField
+          control={form.control}
+          name="whenAreyouLookingToSell"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>When are you looking to sell?</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Listing urgency" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  {listingDuration.map((data) => (
+                    <SelectItem key={data.value} value={data.value}>
+                      {data.label}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </FormItem>
+          )}
+        />
+        <Button className="w-full" type="submit">
+          Next
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+export default PropertyDetails;
