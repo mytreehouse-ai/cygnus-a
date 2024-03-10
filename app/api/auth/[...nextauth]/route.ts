e5199eafc2487aee2b55059929903b3d0d6f8105
casulit <chrisgelosulit@gmail.com>
2024-03-10 11:42:37 +0800

    chore: Remove auth routes and update dependencies

--- app/api/auth/[...nextauth]/route.ts
+++
@@ -1,3 +0,0 @@
-export { GET, POST } from "@/auth";
-// Optional
-// export const runtime = "edge";
--- app/api/session/refresh-token/route.ts
+++
@@ -1,5 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export const GET = async function GET(req: NextRequest) {
-  return NextResponse.json({ message: "Not authenticated" }, { status: 401 });
-};
