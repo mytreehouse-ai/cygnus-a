9d7a8f07674e0ed2ced47554ebf7d44c690f59cf
casulit <chrisgelosulit@gmail.com>
2024-03-10 12:30:46 +0800

    feat(middleware): Add /ai/chat to public routes

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/listings/(.*)", "/valuation", "/contact"],
+  publicRoutes: ["/", "/listings/(.*)", "/valuation", "/contact", "/ai/chat"],
   // ignoredRoutes: ["/", "/listings/(.*)", "/valuation", "/contact"],
 });
 
