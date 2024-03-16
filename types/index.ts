94f41c227ba0aedb71414694504b61308c43f3c5
casulit <chrisgelosulit@gmail.com>
2024-03-16 13:57:09 +0800

    refactor(types): Remove unused ILogin, IAuthToken types

--- types/index.ts
+++ types/index.ts
@@ -1,8 +1,6 @@
 import { IUser } from "./user";
 import { IProfile } from "./profile";
-import { ILogin } from "./login";
 import { IImageKitLoaderParams } from "./imagekit-params";
-import { IAuthToken } from "./auth-token";
 import { IModal } from "./modal";
 
-export type { IUser, IProfile, ILogin, IImageKitLoaderParams, IAuthToken, IModal };
+export type { IUser, IProfile, IImageKitLoaderParams, IModal };
