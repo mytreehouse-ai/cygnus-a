ddf8a46522af3d25658181e210ec9205555b80a7
casulit <chrisgelosulit@gmail.com>
2024-03-17 01:25:02 +0800

    refactor(api-client): Optimize query param handling

--- lib/api-client.ts
+++ lib/api-client.ts
@@ -1,12 +1,35 @@
 import { env } from "@/env.mjs";
 import axios, { InternalAxiosRequestConfig } from "axios";
 
-// Create an Axios instance
 const apiClient = axios.create({
   baseURL: env.NEXT_PUBLIC_OPENRED_BASEAPI_URL,
 });
 
-// Add a request interceptor
+function checkQueryParamsAndClean(config: InternalAxiosRequestConfig<any>) {
+  if (config.params) {
+    Object.keys(config.params).forEach((key) => {
+      if (config.params[key] === null || config.params[key] === undefined) {
+        delete config.params[key];
+      }
+    });
+
+    /**
+     * 1 - for Available
+     * 2 - for Under Offer
+     * 3 - for Sold
+     * 4 - Delisted property
+     */
+    if (!config.params?.property_status) {
+      // Query only all properties that is available.
+      config.params.property_status = 1;
+    }
+
+    if (!config.params?.page_size) {
+      config.params.page_size = 15;
+    }
+  }
+}
+
 apiClient.interceptors.request.use(
   function (config) {
     // Do something before request is sent
@@ -18,27 +41,6 @@ apiClient.interceptors.request.use(
     //   config.headers.Authorization = `Bearer ${token}`;
     // }
 
-    function checkQueryParamsAndClean(config: InternalAxiosRequestConfig<any>) {
-      if (config.params) {
-        Object.keys(config.params).forEach((key) => {
-          if (config.params[key] === null || config.params[key] === undefined) {
-            delete config.params[key];
-          }
-        });
-
-        /**
-         * 1 - for Available
-         * 2 - for Under Offer
-         * 3 - for Sold
-         * 4 - Delisted property
-         */
-        if (!config.params?.property_status) {
-          // Query only all properties that is available.
-          config.params.property_status = 1;
-        }
-      }
-    }
-
     checkQueryParamsAndClean(config);
 
     return config;
@@ -49,7 +51,6 @@ apiClient.interceptors.request.use(
   },
 );
 
-// Add a response interceptor
 apiClient.interceptors.response.use(
   function (response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
--- services/useCitiesQuery.ts
+++ services/useCitiesQuery.ts
@@ -1,30 +1,31 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { type ICity } from "@/types/city";
-import type {
-  IApiBaseResponse,
-  IApiBaseRequestParams,
-} from "@/types/property-api-filters";
+import type { IApiBaseResponse } from "@/types/property-api-filters";
 import { env } from "@/env.mjs";
 import apiClient from "@/lib/api-client";
+import { ReadonlyURLSearchParams } from "next/navigation";
+import { getParams } from "@/lib/utils";
 
-const getCitiesQuery = async (filters: Partial<IApiBaseRequestParams> = {}) => {
+const getCitiesQuery = async (filters?: ReadonlyURLSearchParams) => {
   const url = `${env.NEXT_PUBLIC_OPENRED_BASEAPI_URL}/domains/cities`;
-
+  const params = getParams(filters);
   try {
     const response = await apiClient.get<IApiBaseResponse<ICity[]>>(url, {
-      params: filters,
+      params,
     });
     return response.data;
   } catch (error) {
+    console.error("Failed to fetch cities", error);
     throw new Error("Failed to fetch cities");
   }
 };
 
-const useCitiesQuery = (filters?: Partial<IApiBaseRequestParams>) => {
+const useCitiesQuery = (filters?: ReadonlyURLSearchParams) => {
+  const params = getParams(filters);
   return useInfiniteQuery<IApiBaseResponse<ICity[]>>({
-    queryKey: ["cities", filters || {}],
+    queryKey: ["cities", params],
     queryFn: () => getCitiesQuery(filters),
-    initialPageParam: filters?.page,
+    initialPageParam: params?.page,
     getNextPageParam: (lastPage) => lastPage.next,
   });
 };
