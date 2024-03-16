533e479b71df80ce139f30057a8127b88f294d7c
casulit <chrisgelosulit@gmail.com>
2024-03-16 18:56:06 +0800

    refactor(components): Simplify usePropertiesQuery hook

--- services/usePropertiesQuery.ts
+++ services/usePropertiesQuery.ts
@@ -1,18 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
 import { type IProperty } from "@/types/property";
-import type {
-  IApiBaseResponse,
-  IPropertyFilters,
-} from "@/types/property-api-filters";
+import type { IApiBaseResponse } from "@/types/property-api-filters";
 import { env } from "@/env.mjs";
 import apiClient from "@/lib/api-client";
+import type { ReadonlyURLSearchParams } from "next/navigation";
 
-const getPropertiesQuery = async (filters: Partial<IPropertyFilters> = {}) => {
+const getPropertiesQuery = async (filters?: ReadonlyURLSearchParams) => {
   const url = `${env.NEXT_PUBLIC_OPENRED_BASEAPI_URL}/properties/public`;
+
+  const params = filters ? Object.fromEntries(filters) : {};
 
   try {
     const response = await apiClient.get<IApiBaseResponse<IProperty[]>>(url, {
-      params: filters,
+      params,
     });
     return response.data;
   } catch (error) {
@@ -21,7 +21,7 @@ const getPropertiesQuery = async (filters: Partial<IPropertyFilters> = {}) => {
   }
 };
 
-const usePropertiesQuery = (filters?: Partial<IPropertyFilters>) => {
+const usePropertiesQuery = (filters?: ReadonlyURLSearchParams) => {
   return useQuery<IApiBaseResponse<IProperty[]>>({
     queryKey: ["properties", filters || {}],
     queryFn: () => getPropertiesQuery(filters),
