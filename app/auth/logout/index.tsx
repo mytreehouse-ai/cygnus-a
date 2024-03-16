cf6bfaa216ae04aa271b081b9c51b2848c0d282e
casulit <chrisgelosulit@gmail.com>
2024-03-16 13:51:54 +0800

    chore: Remove obsolete auth components and pages

--- app/auth/logout/index.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { Button } from "@/components/ui/button";
-
-const LogoutButton = () => {
-  return <Button>Sign out</Button>;
-};
-
-export default LogoutButton;
--- types/login.ts
+++
@@ -1,4 +0,0 @@
-export interface ILogin {
-  access: string;
-  refresh: string;
-}
