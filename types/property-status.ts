89233203fee5ad71d45ebbe0cc859bd7fbfd17dc
casulit <chrisgelosulit@gmail.com>
2024-03-17 01:46:56 +0800

    refactor(components): remove unused isMounted state

---
+++ types/property-status.ts
@@ -0,0 +1,5 @@
+export interface PropertyStatus {
+  id: number;
+  description: string;
+  slug: string;
+}
---
+++ types/property-type.ts
@@ -0,0 +1,5 @@
+export interface PropertyType {
+  id: number;
+  description: string;
+  slug: string;
+}
