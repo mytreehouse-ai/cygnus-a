1c1cf3cfc1c896d70648ad0a11df8f77eb3e5644
casulit <chrisgelosulit@gmail.com>
2024-03-17 22:52:15 +0800

    style(gallery): Update image classes for consistency

--- components/listings/details/galery/index.tsx
+++ components/listings/details/galery/index.tsx
@@ -7,44 +7,44 @@ function Galery() {
     <div className="grid grid-cols-1 gap-4 xl:col-span-2 xl:grid-cols-2">
       <div className="relative h-[20rem] xl:h-full">
         <Image
+          className="rounded-lg object-cover"
           src="/dummy-img.png"
           alt="Property image."
           priority={true}
           fill={true}
           sizes="(max-width: 768px) 100vw, 700px"
-          className="rounded-lg"
         />
       </div>
       <div className="xl:grid xl:gap-4">
         <div className="relative hidden xl:inline-block">
           <Image
+            className="rounded-lg"
             src="/dummy-img.png"
             alt="Property image."
             priority={true}
             fill={true}
             sizes="(max-width: 768px) 100vw, 700px"
-            className="rounded-lg"
           />
         </div>
         <div className="grid h-[10rem] grid-cols-2 gap-4 xl:h-full">
           <div className="relative">
             <Image
+              className="rounded-lg"
               src="/dummy-img.png"
               alt="Property image."
               priority={true}
               fill={true}
               sizes="(max-width: 768px) 100vw, 700px"
-              className="rounded-lg"
             />
           </div>
           <div className="relative">
             <Image
+              className="rounded-lg"
               src="/dummy-img.png"
               alt="Property image."
               priority={true}
               fill={true}
               sizes="(max-width: 768px) 100vw, 700px"
-              className="rounded-lg"
             />
             <div className="absolute flex h-full w-full items-center justify-center rounded-lg bg-black/50 text-sm text-white">
               Show all 6 Photos
