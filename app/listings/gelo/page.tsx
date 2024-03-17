9a10a4d3b7b71c1060f05e8db6f566a9d32344df
casulit <chrisgelosulit@gmail.com>
2024-03-17 23:10:18 +0800

    style(listings): Adjust spacing and grid layout

--- app/listings/gelo/page.tsx
+++ app/listings/gelo/page.tsx
@@ -9,7 +9,7 @@ import { MapPin } from "lucide-react";
 
 function PropertyDetails() {
   return (
-    <main className="min-h-screen space-y-8 px-4 py-10">
+    <main className="min-h-screen space-y-4 px-4 py-10">
       <section className="space-y-1 md:col-span-3 ">
         <div className="md:flex md:items-center md:gap-x-2">
           <h3 className="text-2xl font-bold">Furnished Condominium Unit</h3>
@@ -21,15 +21,15 @@ function PropertyDetails() {
         </p>
         <h3 className="text-3xl font-bold text-emerald-600">$146,000</h3>
       </section>
-      <section className="grid gap-y-8 lg:grid-cols-2 lg:gap-x-4 xl:grid-cols-3">
+      <section className="grid lg:grid-cols-2 lg:gap-x-4 xl:grid-cols-3">
         <Galery />
         <Overview />
       </section>
-      <section className="grid gap-y-8 lg:grid-cols-2 lg:gap-x-4">
+      <section className="grid lg:grid-cols-2 lg:gap-x-4 xl:grid-cols-3">
         <Description />
         <Broker />
       </section>
-      <section className="grid gap-y-8 lg:grid-cols-2 lg:gap-x-4">
+      <section className="grid lg:grid-cols-2 lg:gap-x-4">
         <Amenities />
       </section>
     </main>
